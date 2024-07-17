8d25a3cc673c2864ecbe62887134b2bca1500e4a
amadarshprakash <adarshprakash456@gmail.com>
2024-07-17 10:23:11 +0530

    fix:first commit

--- src/components/textBox.js
+++ src/components/textBox.js
@@ -4,7 +4,7 @@ const textBox = () => {
     return (
         <div className='text-box'>
             <h3>JavaScript News and Updates of June 2022 </h3>
-            <h4> Vue Team Resorts to Backporting</h4>
+            <h4> Vue Team Resorts to Backportingc</h4>
             <p>
                 Evan You together with his team gives the finishing touches to Vue 2.7 despite the fact that Vue 3 now being the default version of the framework. Why is that? The point is that many developers still cannot migrate to v3.0 because of various bottlenecks such as dependency compatibility, browser support requirements, etc. Therefore, it was decided to help those who still have to use Vue 2.x by backporting some of the key features from Vue 3. As a result, some features from Vue 3 will be available in v2.7 that is currently in beta.
 
